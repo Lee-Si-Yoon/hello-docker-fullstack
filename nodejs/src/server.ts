5c7d41ed1f099c243014d9acf8171c878edee2b8
leesiyoon <siyoon@friendli.ai>
2024-09-17 16:44:56 +0900

    try isolated-vm

--- nodejs/src/server.ts
+++ nodejs/src/server.ts
@@ -1,11 +1,49 @@
 import express from "express";
 import bodyParser from "body-parser";
+import ivm from "isolated-vm";
 
 const app = express();
 app.use(bodyParser.json());
 
 app.get("/nodejs/hi", function (req, res) {
   res.status(200).send({ ok: true });
+});
+
+app.post("/nodejs/run", async function (req, res) {
+  const { code } = req.body;
+
+  let consoleOutput: string[] = [];
+
+  const logCallback = function (...args: any[]) {
+    consoleOutput.push(args.join(" "));
+  };
+
+  try {
+    const isolate = new ivm.Isolate({ memoryLimit: 128 });
+    const context = isolate.createContextSync();
+    const jail = context.global;
+
+    jail.setSync("global", jail.derefInto());
+    jail.setSync("log", function (...args: any) {
+      console.log(...args);
+    });
+
+    const script = await isolate.compileScriptSync(`
+      (function() {
+        ${code}
+      })();
+    `);
+
+    await script.run(context);
+
+    res.status(200).send({
+      console: consoleOutput,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .send({ error: (error as Error).message, console: consoleOutput });
+  }
 });
 
 app.listen(5000, () => {
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,10 @@
 import React from "react";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
 function App() {
+  const [nodeJsCode, setNodeJsCode] = React.useState(`console.log("hello")`);
+  const [pythonCode, setPythonCode] = React.useState(`print("hello")`);
+
   const {
     isPending: nodejsPending,
     isError,
@@ -12,6 +15,24 @@ function App() {
       fetch("/nodejs/hi").then(async (res) => {
         return "good";
       }),
+  });
+
+  const {
+    mutate: runNodeJsCode,
+    data: nodeJsRunReturns,
+    isPending: nodeJsRunPending,
+  } = useMutation({
+    mutationFn: (code: string) => {
+      return fetch("/nodejs/run", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ code }),
+      }).then(async (res) => {
+        return res.json();
+      });
+    },
   });
 
   const { isPending: pythonPending } = useQuery({
@@ -30,7 +51,56 @@ function App() {
     return <span>Error: {error.message}</span>;
   }
 
-  return <header>hello world</header>;
+  return (
+    <main style={{ display: "flex", columnGap: 8 }}>
+      <section style={{ flex: 1 }}>
+        <form
+          id="nodejs"
+          onSubmit={(e) => {
+            e.preventDefault();
+            runNodeJsCode(nodeJsCode);
+          }}
+          style={{ display: "flex", flexDirection: "column" }}
+        >
+          <label htmlFor="nodejs-code">nodejs code</label>
+          <textarea
+            form="nodejs"
+            id="nodejs-code"
+            name="nodejs-code"
+            value={nodeJsCode}
+            onChange={(e) => setNodeJsCode(e.target.value)}
+            wrap="hard"
+            rows={5}
+          />
+          <button type="submit" disabled={nodeJsRunPending}>
+            run
+          </button>
+        </form>
+      </section>
+
+      <section style={{ flex: 1 }}>
+        <form
+          id="python"
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+          style={{ display: "flex", flexDirection: "column" }}
+        >
+          <label htmlFor="python-code">python code</label>
+          <textarea
+            form="python"
+            id="python-code"
+            name="python-code"
+            value={pythonCode}
+            onChange={(e) => setPythonCode(e.target.value)}
+            wrap="hard"
+            rows={5}
+          />
+          <button type="submit">run</button>
+        </form>
+      </section>
+    </main>
+  );
 }
 
 export default App;
